4777c9bad12f701fddf0825ad961460169bd4b0f
weslley070 <137814164+weslley070@users.noreply.github.com>
2024-03-16 14:07:02 -0300

    exercicios

--- exercicios/adivinhanum/scripts/script.js
+++ exercicios/adivinhanum/scripts/script.js
@@ -1,15 +1,4 @@
-let num_random = Math.floor(Math.random()* 100)
-let teste = document.getElementById('t')
-function verificar(){
-    let n1 = Number(document.getElementById('txt').value)
-    let res = document.getElementById('res')
-    if(n1 < num_random){
-        res.innerHTML = `O número ${n1} é menor!`
-    }else if(n1 > num_random){
-        res.innerHTML = `O número ${n1} é menor!`
-    }else{
-        res.innerHTML = `Parabéns! ${n1} é o número!`
-    }
-    
-}
-teste.innerHTML = num_random
+let res = document.getElementById('res')
+let n1 = Number(document.getElementById('txt1').value)
+let num_alea = Math.floor(Math.random() * 100)
+res.innerHTML = num_alea
